344432c53c2fc3fccb88ec40c7c941651820a9cd
choongnyeong6215 <choong6215@gmail.com>
2025-03-18 20:59:36 +0900

    [Reactor] 토큰 미들웨어 로직 개선

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,40 +1,66 @@
+import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
-import * as express from "express";
 import RefreshTokenModel from "../models/token.model";
 import { User } from "../types/user.type";
+import { PROVIDER } from "../constants";
 
-const generateAccessToken = (user: User) => {
+export const generateAccessToken = (user: User) => {
   const payload = {
-    id: user._id,
+    oauthId: user.oauthId,
     email: user.email,
   };
 
-  return jwt.sign(payload, process.env.JWT_SECRET_KEY as string, {
-    expiresIn: "10m",
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: "30m",
   });
 };
 
-const generateRefreshToken = async () => {
-  const refreshToken = jwt.sign({}, process.env.JWT_SECRET_KEY as string, {
-    expiresIn: "7d",
-  });
-
-  await RefreshTokenModel.create({
-    token: refreshToken,
-  });
-
-  return refreshToken;
-};
-
-const verifyAccessToken = (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
+export const generateRefreshToken = async (
+  provider: string,
+  oauthId: string,
+  refreshToken?: string
 ) => {
+  let token;
+
+  const payload = {
+    oauthId,
+  };
+
+  if (provider === PROVIDER.local) {
+    token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+  } else if (provider === PROVIDER.kakao) {
+    token = refreshToken;
+  }
+
+  await RefreshTokenModel.findOneAndUpdate(
+    {
+      oauthId,
+    },
+    {
+      token,
+    },
+    {
+      new: true,
+      upsert: true,
+    }
+  );
+
+  return token;
+};
+
+export const validateAccessToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
   const authHeader = req.headers["authorization"];
 
+  console.log(authHeader);
+
   if (!authHeader) {
-    return res.status(401).json({
+    res.status(401).json({
       message: "토큰이 존재하지 않습니다.",
     });
   }
@@ -42,87 +68,83 @@ const verifyAccessToken = (
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     req.user = decoded;
 
-    return next();
+    next();
   } catch (err) {
     if (err instanceof jwt.TokenExpiredError) {
-      console.log("유효기간이 만료된 토큰입니다.");
-
-      return refreshAccessToken(req, res, next);
+      res.status(401).json({
+        message: "유효기간이 만료된 토큰입니다.",
+      });
     }
 
-    return res.status(403).json({
+    res.status(401).json({
       message: "유효하지 않은 토큰입니다.",
     });
   }
 };
 
-const refreshAccessToken = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-): Promise<any> => {
-  const refreshToken = req.headers["refresh-token"];
+export const reIssueAccessToken = async (
+  req: Request<{}, {}, { oauthId: string }>,
+  res: Response
+) => {
+  const { oauthId } = req.body;
+
+  if (!oauthId) {
+    return res.status(401).json({
+      message: "사용자 id가 존재하지 않습니다.",
+    });
+  }
+
+  const refreshToken = await getRefreshToken(oauthId);
 
   if (!refreshToken) {
     return res.status(401).json({
-      message: "refresh 토큰이 존재하지 않습니다.",
+      message: "리프레시 토큰이 존재하지 않습니다.",
     });
   }
 
   try {
-    const existingRefreshToken = await RefreshTokenModel.findOne({
-      token: refreshToken,
-    });
+    const decoded = jwt.verify(
+      refreshToken,
+      process.env.JWT_SECRET_KEY
+    ) as jwt.JwtPayload;
 
-    if (!existingRefreshToken) {
-      return res.status(403).json({
-        message: "유효하지 않은 refresh 토큰입니다.",
+    if (!decoded.oauthId || !decoded.email) {
+      return res.status(401).json({
+        message: "유효하지 않은 리프레시 토큰입니다.",
       });
     }
 
-    const newAccessToken = generateAccessToken(req.user as User);
+    const payload = {
+      oauthId: decoded.oauthId,
+      email: decoded.email,
+    };
+
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: "10m",
+    });
 
     return res.status(200).json({
-      accessToken: newAccessToken,
+      accessToken,
     });
   } catch (err) {
-    return res.status(403).json({
-      message: "refresh 토큰 검증 실패",
-    });
-  }
-};
+    await removeRefreshToken(oauthId);
 
-const removeRefeshToken = async (
-  req: express.Request,
-  res: express.Response
-) => {
-  const refreshToken = req.headers["refresh-token"];
-
-  if (!refreshToken) {
     return res.status(401).json({
-      message: "refresh 토큰이 존재하지 않습니다.",
+      message: "유효하지 않은 리프레시 토큰입니다.",
     });
   }
-
-  try {
-    await RefreshTokenModel.deleteOne({
-      token: refreshToken,
-    });
-
-    return res.status(200).json({
-      message: "refresh 토큰 삭제 완료",
-    });
-  } catch (err) {}
 };
 
-export {
-  generateAccessToken,
-  generateRefreshToken,
-  verifyAccessToken,
-  refreshAccessToken,
-  removeRefeshToken,
+export const getRefreshToken = async (oauthId: string) => {
+  const refreshToken = await RefreshTokenModel.findOne({ oauthId });
+
+  return refreshToken.token;
+};
+
+export const removeRefreshToken = async (oauthId: string) => {
+  return await RefreshTokenModel.deleteOne({ oauthId });
 };
