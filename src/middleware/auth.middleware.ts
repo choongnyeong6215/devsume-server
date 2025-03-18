8beeffd15cd7789c54300d87524cac8485598b39
choongnyeong6215 <choong6215@gmail.com>
2025-03-18 22:07:21 +0900

    [Modify] 카카오 엑세스 토큰 재발급 로직 추가

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,6 +3,8 @@ import * as jwt from "jsonwebtoken";
 import RefreshTokenModel from "../models/token.model";
 import { User } from "../types/user.type";
 import { PROVIDER } from "../constants";
+import axios from "axios";
+import { KakaoRefreshResponse } from "../types/token.type";
 
 export const generateAccessToken = (user: User) => {
   const payload = {
@@ -56,8 +58,6 @@ export const validateAccessToken = (
   next: NextFunction
 ): void => {
   const authHeader = req.headers["authorization"];
-
-  console.log(authHeader);
 
   if (!authHeader) {
     res.status(401).json({
@@ -147,4 +147,36 @@ export const getRefreshToken = async (oauthId: string) => {
 
 export const removeRefreshToken = async (oauthId: string) => {
   return await RefreshTokenModel.deleteOne({ oauthId });
+};
+
+export const reIssueKakaoAccessToken = async (refreshToken: string) => {
+  const existingToken = await RefreshTokenModel.findOne({
+    token: refreshToken,
+  });
+
+  if (!existingToken) {
+    throw new Error("가입하지 않은 사용자입니다.");
+  }
+
+  try {
+    const response = await axios.post<KakaoRefreshResponse>(
+      "https://kauth.kakao.com/oauth/token",
+      {
+        grant_type: "refresh_token",
+        client_id: process.env.KAKAO_CLIENT_ID,
+        refresh_token: refreshToken,
+        client_secret: process.env.KAKAO_CLIENT_SECRET,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        },
+      }
+    );
+    return {
+      accessToken: response.data.access_token,
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
