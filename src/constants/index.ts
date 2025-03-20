71d2d9bf429f50dde70adf26c26b3f483b014c23
choongnyeong6215 <choong6215@gmail.com>
2025-03-20 23:29:23 +0900

    [Modify] 이력서 상태 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,12 @@
+import { ResumeStatus } from "../types/resume.type";
 import { Provider } from "./../types/user.type";
 
 export const PROVIDER: Record<Provider, Provider> = {
   local: "local",
   kakao: "kakao",
+};
+
+export const STATUS: Record<ResumeStatus, ResumeStatus> = {
+  temporary: "temporary",
+  completed: "completed",
 };
