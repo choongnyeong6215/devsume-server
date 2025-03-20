b51cf03e71a7b4093b37333c0ed32aff8593b45b
choongnyeong6215 <choong6215@gmail.com>
2025-03-20 23:31:31 +0900

    [Add] 패키지 환경변수 타입 추가 정의

--- src/types/express/env.d.ts
+++ src/types/express/env.d.ts
@@ -5,5 +5,9 @@ declare namespace NodeJS {
     JWT_SECRET_KEY: string;
     KAKAO_CLIENT_ID: string;
     KAKAO_CLIENT_SECRET: string;
+    AWS_REGION: string;
+    S3_BUCKET_NAME: string;
+    S3_ACCESS_KEY_ID: string;
+    S3_ACCESS_SECRET_KEY: string;
   }
 }
