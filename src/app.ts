2cef29972b6f8412b0e06a105a615582c4560057
choongnyeong6215 <choong6215@gmail.com>
2025-03-21 22:56:34 +0900

    [Modify] Passport & Error 미들웨어 적용 to App Module

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,36 @@ dotenv.config({ path: "src/config/.env" });
 
 import express from "express";
 import connectDb from "./config/db.ts";
+import userRouter from "./routes/user.route.ts";
+import resumeRouter from "./routes/resume.route.ts";
+import * as errorMiddleware from "./middleware/error.middleware.ts";
+import passport from "passport";
+import localStrategy from "./strategies/local.strategy.ts";
+import kakaoStrategy from "./strategies/kakao.strategy.ts";
 
 const app = express();
 const PORT = process.env.PORT;
 
-// console.log();
 // db connection
 connectDb();
 
-// middleware
+// passport stragtegy
+localStrategy();
+kakaoStrategy();
+
+// common middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// passport
+app.use(passport.initialize());
+
+// end point
+app.use("/users", userRouter);
+app.use("/resumes", resumeRouter);
+
+// error middleware
+app.use(errorMiddleware.errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
